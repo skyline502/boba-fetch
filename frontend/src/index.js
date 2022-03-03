62a503e9830b61ebba763b6d1fa14c05c520664b
Johnny San <johnny.san@gmail.com>
2022-03-03 15:17:13 -0800

    added csrf to frontend and backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
+//front/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import App from './App';
 
 import configureStore from './store';
@@ -10,8 +13,13 @@ import configureStore from './store';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
+
 
 const Root = () => {
   return (
