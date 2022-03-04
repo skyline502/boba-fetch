31ac7fc5bc32427a4c23d101dfd125813823057a
Johnny San <johnny.san@gmail.com>
2022-03-04 15:38:46 -0800

    log out log in sign up buttons appear and disappear depending on session user

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,7 +1,28 @@
 import { NavLink, Link } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux';
+import { logOut } from '../../store/session';
 import './Navigation.css';
 
 const Navigation = () => {
+    const sessionUser = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
+
+    let sessionLinks;
+
+    if (sessionUser) {
+        sessionLinks = (
+            <button onClick={() => dispatch(logOut())}>Log Out</button>
+        );
+    } else {
+        sessionLinks = (
+            <div className="buttons-container">
+
+                <Link to='/login'><button className="log-in">Login</button></Link>
+                <Link to='/signup'><button className="sign-up">Sign Up</button></Link>
+            </div>
+        );
+    }
+
     return (
         <nav className="nav-bar">
             <div className="logo">
@@ -15,10 +36,7 @@ const Navigation = () => {
                 <input type='text'></input>
                 <img src='/images/search.png' className="search-img"></img>
             </div>
-            <div className="buttons-container">
-                <Link to='/login'><button className="log-in">Login</button></Link>
-                <Link to='/signup'><button className="sign-up">Sign Up</button></Link>
-            </div>
+            {sessionLinks}
         </nav>
     )
 }
