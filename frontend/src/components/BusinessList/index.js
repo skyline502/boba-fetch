030f1826b8668f953fd910abe1e79f453b9f24a1
Johnny San <johnny.san@gmail.com>
2022-03-07 13:07:50 -0800

    get one business done

--- frontend/src/components/BusinessList/index.js
+++ frontend/src/components/BusinessList/index.js
@@ -7,38 +7,71 @@ const BusinessList = () => {
   const sessionUser = useSelector(state => state.session.user);
   const shops = useSelector(state => state.businesses);
   const dispatch = useDispatch();
-
   const list = shops.businesses;
+  const [selectedShop, setSelectedShop] = useState(shops.businesses[0]);
+  const [selectedId, setSelectedId] = useState('');
+
+  console.log('selected: ', selectedShop);
+  console.log('selectedId:', selectedId);
+  useEffect(() => {
+    const shop = list.find(shop => shop.id === selectedId);
+    setSelectedShop(shop);
+  }, [selectedId])
+
 
   useEffect(() => {
     dispatch(getBusinesses());
+    setSelectedShop(list[0]);
   }, [dispatch]);
 
+  let selected;
 
-  return (
-    <div className='shop-list-container'>
+  if (selectedShop) {
+    selected = (
+      <div className='selected-shop'>
+        <img src={selectedShop.businessImg} style={{ width: 400 }} />
+        <div className="selected-shop-info">
+          <h1>{selectedShop.name}</h1>
+          <h4>{selectedShop.address}</h4>
+          <h4>{selectedShop.city}, {selectedShop.state} {selectedShop.zipCode}</h4>
+          <h4>Phone: ({selectedShop.phone.split('').slice(0, 3)}) {selectedShop.phone.split('').slice(3, 6)}-{selectedShop.phone.split('').slice(6)}</h4>
+          <p style={{fontWeight: 'lighter'}}>{selectedShop.description}</p>
+        </div>
+      </div>
+    );
+  } else {
+    selected = (
       <div className='selected-shop'>
         <div className='shop-header'>
           <h1>Welcome to Boba Fetch!</h1>
         </div>
-        <img src="/images/baby-yoda-eggs.gif" />
-        <div className="profile-header">
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className='shop-list-container'>
+        {selected}
+        <div className='shop-list'>
+          {list.map(shop => (
+            <div key={shop.id} className="shop-box">
+              <img
+              src={shop.businessImg}
+              style={{ width: 200 }}
+              onClick={ () => setSelectedId(shop.id) }
+              />
+              <div className='shop-info'>
+                <h2>{shop.name}</h2>
+                <h6>{shop.address}</h6>
+                <h6>{shop.city}, {shop.state} {shop.zipCode}</h6>
+                <h6>Phone: ({shop.phone.split('').slice(0, 3)}) {shop.phone.split('').slice(3, 6)}-{shop.phone.split('').slice(6)}</h6>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div className='shop-list'>
-        {list.map(shop => (
-          <div key={shop.id} className="shop-box">
-            <img src={shop.businessImg} style={{ width: 200 }} />
-            <div className='shop-info'>
-              <h2>{shop.name}</h2>
-              <h6>{shop.address}</h6>
-              <h6>{shop.city}, {shop.state} {shop.zipCode}</h6>
-              <h6>Phone: ({shop.phone.split('').slice(0, 3)}) {shop.phone.split('').slice(3, 6)}-{shop.phone.split('').slice(6)}</h6>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   )
 }
 
