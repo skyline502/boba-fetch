39344e56421e32b846284c6082e65725a955232d
Johnny San <johnny.san@gmail.com>
2022-03-08 13:48:55 -0800

    created review migration

---
+++ backend/db/models/review.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Review = sequelize.define('Review', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    businessId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: 1,
+        max: 5
+      },
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  }, {});
+  Review.associate = function(models) {
+    Review.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'cascade', foreignKeyConstraint: true });
+    Review.belongsTo(models.Business, { foreignKey: 'businessId', onDelete: 'cascade', foreignKeyConstraint: true });
+  };
+  return Review;
+};
---
+++ backend/db/seeders/20220308214524-reviews.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Reviews', [
+     {
+       userId: '1',
+       businessId: '2',
+       rating: 3,
+       review: 'Tea is so so, go somewhere else.'
+     }
+   ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Reviews', null, {});
+  }
+};
