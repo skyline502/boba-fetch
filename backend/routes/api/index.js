24feee2f4a2c259cf10635c69c14e0a5085dcff5
Johnny San <johnny.san@gmail.com>
2022-03-08 14:04:18 -0800

    backend route for get reviews works!

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 const sessionRouter = require('./session');
 const usersRouter = require('./users');
 const businessesRouter = require('./businesses.js');
+const reviewsRouter = require('./reviews');
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/businesses', businessesRouter);
+router.use('/reviews', reviewsRouter);
 
 
 //routes
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { Review } = require('../../db/models');
+
+const router = express.Router();
+
+const validateReview = [
+  check('rating')
+    .exists({ checkFalsy: true })
+    .isNumeric()
+    .withMessage('Please select a rating between 1 and 5'),
+  check('review')
+    .exists({ checkFalsy: true })
+    .withMessage('Please leave a review'),
+  handleValidationErrors
+]
+
+//ROUTES
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  const businessId = req.params.id
+  const reviews = await Review.findAll({
+    where: {
+      businessId
+    }
+  });
+  console.log('review list: ', reviews);
+  return res.json(reviews);
+}))
+
+module.exports = router;
