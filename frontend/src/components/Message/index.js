e8019451d97c82f4f6b6468f2185a0be784a359a
Johnny San <johnny.san@gmail.com>
2022-03-09 12:58:56 -0800

    added styling to delete message

--- frontend/src/components/Message/index.js
+++ frontend/src/components/Message/index.js
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import './Message.css';
 
 const Message = () => {
   return (
-    <>
-      <h1>Success!</h1>
-      <NavLink to='/businesses'><button>Go back</button></NavLink>
-    </>
+    <div className="message-container">
+      <div className="message-header">
+        <h1>Business Deleted Successfully!</h1>
+      </div>
+      <img style={{borderRadius: 15, margin: 10}} id='goodbye' src='/images/baby-yoda-grogu.gif'/>
+      <NavLink to='/businesses'><button className="back-button">Go back</button></NavLink>
+    </div>
   )
 }
 
