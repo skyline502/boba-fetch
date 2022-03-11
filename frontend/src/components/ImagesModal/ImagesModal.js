2bc0a7c566f607c4a098e57944f391caa68ad8de
Johnny San <johnny.san@gmail.com>
2022-03-11 10:51:01 -0800

    added create image form

--- frontend/src/components/ImagesModal/ImagesModal.js
+++ frontend/src/components/ImagesModal/ImagesModal.js
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react';
+
 const ImagesContainer = ({images}) => {
+  const [form, setForm] = useState('hide-form');
+  const [showForm, setShowForm] = useState(false);
+  const [imgUrl, setImgUrl] = useState();
+
+  useEffect(() => {
+    if (showForm) {
+      setForm('show-form');
+    } else {
+      setForm('hide-form');
+    }
+  }, [showForm])
   return (
     <div className="images-container">
       <h1>Images</h1>
+      <div className="add-img-form">
+        <button onClick={() => setShowForm(!showForm)}>Add Image</button>
+        <form className={`add-image-form ${form}`}>
+          <input
+          type='url'
+          placeholder='enter image url'
+          value={imgUrl}
+          onChange={e => setImgUrl(e.target.value)}
+          />
+          <button type="submit">submit</button>
+        </form>
+      </div>
       {images.map(image => (
-        <img key={image.id} src={image.imgUrl} />
+        <div className="str-img-box" key={image.id}>
+          <img src={image.imgUrl} />
+        </div>
       ))}
     </div>
   )
